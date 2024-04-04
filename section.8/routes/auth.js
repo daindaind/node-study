b296b771db80aae68d760997a4cc3a9275b94e32
chaKK <cdin23@naver.com>
2024-04-05 02:36:00 +0900

    section.8 카카오 로그인 구현

--- section.8/routes/auth.js
+++ section.8/routes/auth.js
@@ -10,4 +10,15 @@ router.post("/join", isNotLoggedIn, join);
 router.post("/login", isNotLoggedIn, login);
 router.get("/logout", isLoggedIn, logout);
 
+router.get("/kakao", passport.authenticate("kakao"));
+router.get(
+  "/kakao/callback",
+  passport.authenticate("kakao", {
+    failureMessage: "카카오로그인 실패!",
+  }),
+  (req, res) => {
+    res.redirect("http://localhost:5173");
+  }
+);
+
 module.exports = router;
